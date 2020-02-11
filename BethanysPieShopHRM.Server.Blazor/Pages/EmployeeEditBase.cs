5aa76ee82799e74dace018e002fe8c7c8f2cf3a3
MohamedAshraf004 <lionelmido004@gmail.com>
2020-02-11 20:28:33 +0200

    Create && Edit and Delete Was Added

--- BethanysPieShopHRM.Server.Blazor/Pages/EmployeeEditBase.cs
+++ BethanysPieShopHRM.Server.Blazor/Pages/EmployeeEditBase.cs
@@ -17,15 +17,8 @@ namespace BethanysPieShopHRM.Server.Blazor.Pages
 		public ICountryDataService CountryDataService { get; set; }
 		[Inject]
 		public IJobCategoryDataService JobCategoryDataService { get; set; }
-		protected override async Task OnInitializedAsync()
-		{
-			Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
-			Countries = (await CountryDataService.GetAllCountries()).ToList();
-			JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
-			CountryId = Employee.CountryId.ToString();
-			JobCategoryId = Employee.JobCategoryId.ToString();
-		}
-
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
 
 		[Parameter]
 		public string EmployeeId { get; set; }
@@ -35,5 +28,72 @@ namespace BethanysPieShopHRM.Server.Blazor.Pages
 
 		protected string CountryId = string.Empty;
 		protected string JobCategoryId = string.Empty;
+
+		protected string Message = string.Empty;
+		protected string StatusClass = string.Empty;
+		protected bool Saved;
+
+		protected override async Task OnInitializedAsync()
+		{
+			Saved = false;
+			//Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+			Countries = (await CountryDataService.GetAllCountries()).ToList();
+			JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+			int.TryParse(EmployeeId,out var employeeId);
+			if(employeeId == 0)
+			{
+				Employee = new Employee() { CountryId = 1, JobCategoryId = 0, BirthDate = DateTime.Now, JoinedDate = DateTime.Now };
+
+			}
+			else
+			{
+				Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+			}
+			CountryId = Employee.CountryId.ToString();
+			JobCategoryId = Employee.JobCategoryId.ToString();
+		}
+
+		protected async Task HandleValidSubmit()
+		{
+			Saved = false;
+			Employee.CountryId = int.Parse(CountryId);
+			Employee.JobCategoryId = int.Parse(JobCategoryId);
+			if(Employee.EmployeeId == 0)
+			{
+				var addedEmployee = await EmployeeDataService.AddEmployee(Employee);
+				if(addedEmployee != null)
+				{
+					StatusClass = "alert-success";
+					Message = "New employee added successfully.";
+					Saved = true;
+				}
+				else
+				{
+					StatusClass = "alert-danger";
+					Message = "Something went wrong adding the new employee. Please try again.";
+					Saved = false;
+				}
+			}
+			else
+			{
+				await EmployeeDataService.UpdateEmployee(Employee);
+				StatusClass = "alert-success";
+				Message = "Employee updated successfully.";
+				Saved = true;
+			}
+		}
+		protected async Task DeleteEmployee()
+		{
+			await EmployeeDataService.DeleteEmployee(Employee.EmployeeId);
+			StatusClass = "alert-success";
+			Message = "Deleted successfully";
+			Saved = true;
+		}
+		protected void NavigateToOverview()
+		{
+			NavigationManager.NavigateTo("/employeeoverview");
+		}
+
 	}
+
 }
--- BethanysPieShopHRM.Server.Blazor/Services/EmployeeDataService.cs
+++ BethanysPieShopHRM.Server.Blazor/Services/EmployeeDataService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -16,16 +17,7 @@ namespace BethanysPieShopHRM.Server.Blazor.Services
         {
             this._httpClient = httpClient;
         }
-        public Task<Employee> AddEmployee(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteEmployee(int employeeId)
-        {
-            throw new NotImplementedException();
-        }
-
+        
         public async Task<IEnumerable<Employee>> GetAllEmployees()
         {
             return await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
@@ -40,9 +32,30 @@ namespace BethanysPieShopHRM.Server.Blazor.Services
                            new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
-        public Task UpdateEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("api/employee", employeeJson);
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+            }
+            return null;
         }
+
+        public async Task UpdateEmployee(Employee employee)
+        {
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            await _httpClient.PutAsync("api/employee", employeeJson);
+        }
+
+        public async Task DeleteEmployee(int employeeId)
+        {
+            await _httpClient.DeleteAsync($"api/employee/{employeeId}");
+        }
+
+
     }
 }
